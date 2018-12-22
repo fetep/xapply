35605a0e3463f27c95ab71847305746d5a91fddb
Pete Fritchman <petef@databits.net>
2018-12-22 19:57:03 +0000

    stdin things

--- xapply.go
+++ xapply.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/fetep/xapply/dicer"
 )
+
+func apply(dicerTmpl dicer.Template, input string) {
+	output, err := dicerTmpl.Expand([]string{input})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", output)
+}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -16,10 +25,13 @@ func main() {
 	cmd, args := os.Args[1], os.Args[2:]
 	dicerTmpl := dicer.NewTemplate(cmd)
 	for _, input := range args {
-		diced, err := dicerTmpl.Expand([]string{input})
-		if err != nil {
-			panic(err)
+		if input == "-" {
+			scanner := bufio.NewScanner(os.Stdin)
+			for scanner.Scan() {
+				apply(dicerTmpl, scanner.Text())
+			}
+		} else {
+			apply(dicerTmpl, input)
 		}
-		fmt.Printf("%s\n", diced)
 	}
 }
